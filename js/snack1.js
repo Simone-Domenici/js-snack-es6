cd9e2524ce3b501c9dc9c4b4387debec74aa6bd3
Simone-Domenici <domenici.simone00@gmail.com>
2024-10-25 14:55:54 +0200

    ADD 1st exercise

--- js/snack1.js
+++ js/snack1.js
@@ -4,3 +4,32 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore.
 */
+
+const bikeShop = [
+    bikeOne = {
+        nome: 'Saetta',
+        peso: 25,
+    },
+    bikeTwo = {
+        nome: 'Mcqueen',
+        peso: 31,
+    },
+    bikeThree = {
+        nome: 'Graziella',
+        peso: 18,
+    }
+]
+
+// console.log(bikeShop)
+let result = {
+    nome:'',
+    peso:9999,
+}
+for (let i = 0; i < bikeShop.length; i++) {
+    if (bikeShop[i].peso < result.peso) {
+        result.nome = bikeShop[i].nome
+        result.peso = bikeShop[i].peso
+    }
+}
+
+console.log(result)
